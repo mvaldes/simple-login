59e990f33e9ed8839a0a9613be5322f42b90ac59
Mauro Valdes <tzn589@gmail.com>
2023-10-23 16:26:50 -0400

    adding login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { green } from "@mui/material/colors";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 function App() {
+  const [user, setUser] = useState(0);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    setUser(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className="form">
+      <div className="container">
+        {user === 0 ? (
+          <>
+            <div className="header">
+              <div className="text">Login</div>
+            </div>
+            <div className="inputs-container">
+              <div className="input">
+                <label>Nom d'utilisateur:</label>
+                <input
+                  placeholder="Entrer votre nom d'utilisateur"
+                  type="text"
+                  name="username"
+                ></input>
+              </div>
+
+              <div className="input">
+                <label>Mot de passe:</label>
+                <input
+                  placeholder="Entrer votre mot de passe"
+                  name="password"
+                  type="password"
+                ></input>
+              </div>
+
+              <div className="submit-container">
+                <button className="submit" onClick={() => handleSubmit}>
+                  Login
+                </button>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="congrats">
+              <div>
+                <div className="icon">
+                  <CheckCircleOutlineIcon
+                    sx={{ color: green[500], fontSize: 160 }}
+                  />
+                </div>
+                <div>Vous êtes maintenant Logger</div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </form>
   );
 }
 
